559ae0acc5a210fdcf39734b3065f3b1b97b1b61
HilalVildan <hilalsarioglu96@gmail.com>
2022-12-12 15:14:20 +0100

    update

--- app.js
+++ app.js
@@ -1,80 +1,70 @@
-//!hesapla butonuna tıklandığında öncelikle preventDefault() komutu şart, 
-//!onu yazmazsak girilen bilgilerle ekrana çıkarmak istediğim veriler çıkmaz, 
-//!çünkü index.html deki form, aldığı bilgileri backend e yollamaya programlı, 
+//!hesapla butonuna tıklandığında öncelikle preventDefault() komutu şart,
+//!onu yazmazsak girilen bilgilerle ekrana çıkarmak istediğim veriler çıkmaz,
+//!çünkü index.html deki form, aldığı bilgileri backend e yollamaya programlı,
 //!önce benim işimi gör diyoruz preventDefault diyerek.
 
-const hesaplaButon = document.querySelector(".btn");
-const vade = document.querySelector("#vade")
-const tutar = document.querySelector("#tutar")
-let oran = 0;
-let taksit;
+const calculateButon = document.querySelector(".btn");
+const maturity = document.querySelector("#maturity");
+const amount = document.querySelector("#amount");
+let rate = 0;
+let installment;
 
-
-hesaplaButon.addEventListener("click", (e) => {
-  //*form işlemlerinde formun submit inin default kendi hareketlerini yapmaması, 
+calculateButon.addEventListener("click", (e) => {
+  //*form işlemlerinde formun submit inin default kendi hareketlerini yapmaması,
   //*benim istediklerimi yapması için e.preventDefault() komutunu kullanırız
 
   e.preventDefault();
 
   if (document.querySelector(".form-select").value === "Housing loan") {
-    oran = 1.29;
+    rate = 1.29;
   } else if (
     document.querySelector(".form-select").value === "Personal finance credit"
   ) {
-    oran = 1.99;
+    rate = 1.99;
   } else if (document.querySelector(".form-select").value === "Auto loan") {
-    oran = 1.79;
+    rate = 1.79;
   }
 
+  const faiz = rate / 100;
 
+  console.log(faiz);
 
-const faiz = oran /100;
+  //ekrana bastirilacak taksit amounti hesaplama formulu
 
-console.log(faiz);
+  installment =
+    (amount.value * (faiz * (1 + faiz) ** maturity.value)) /
+    ((1 + faiz) ** maturity.value - 1);
 
-//ekrana bastirilacak taksit tutari hesaplama formulu
-
-
-taksit = (tutar.value * (faiz * (1+ faiz) ** vade.value)) / ((1 + faiz) ** vade.value -1)
-
-console.log(taksit);
-sonuclariGöster()
-
-})
+  console.log(installment);
+  showResult();
+});
 //ekrana basilacak tablo
 
+const showResult = () => {
+  const result = document.querySelector(".result");
 
-const sonuclariGöster = () => {
-
-    const sonuclar = document.querySelector(".sonuclar");
-
-    sonuclar.innerHTML = 
-    ` <h2 class="mt-3 text-warning">Credit information</h2>
+  result.innerHTML = ` <h2 class="mt-3 text-warning">Credit information</h2>
   <table class="table table-bordered border-warning  mt-4">
    <tbody>
     <tr>
       <th>Credit amount</th>
-      <td>${tutar.value} ₺</td>
+      <td>${amount.value} ₺</td>
       <th>Credit Type</th>
       <td>${select.value}</td>
     </tr>
     <tr>
       <th>Maturity</th>
-      <td>${vade.value}</td>
+      <td>${maturity.value}</td>
       <th>Interest rate</th>
-      <td>${oran}</td>
+      <td>${rate}</td>
     </tr>
     <tr>
       <th>Total amount</th>
-      <td>${(taksit * vade.value).toFixed(2)} ₺</td>
+      <td>${(installment * maturity.value).toFixed(2)} ₺</td>
       <th>Installment amount</th>
-      <td>${taksit.toFixed(2)} ₺</td>
+      <td>${installment.toFixed(2)} ₺</td>
     </tr>
   </tbody>
 
-</table>`
-}
-
-
-
-
+</table>`;
+};
